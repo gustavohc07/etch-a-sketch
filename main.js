54e6833fb95b3569edb10e0e2fd2ec556b693eb5
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2019-09-01 19:03:58 -0300

    Changed flexbox to grid

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 //Buttons
 
 //Create Grid
-
-for (i = 0; i < (16*16); i++) {
-    let grid = document.createElement('div');
-    grid.classList.add('grid__element')
-    document.querySelector(".grid").appendChild(grid);
+let j = 0
+while (j<16) {
+    for (i = 0; i < 16; i++) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid__element', 'grid__element--hover', 'grid__element--color')
+        document.querySelector(".grid").appendChild(grid);
+    }
+    j++
 }
+
+function setBlack() {
+    const gridElement = document.querySelector(".grid__element--color");
+    gridElement.setAttribute("style", "background-color: black;")
+}
+
+// function getRandomColor() {
+
+// }
+
+// function getSelectedColor() {
+
+// }
+
+// function reset {
+
+// }
+
+
+
+const blackColor = document.querySelectorAll(".grid__element")
+for (i = 0; i < blackColor.length; i++) {
+    const divs = blackColor[i];
+    divs.addEventListener('mouseover', )
+}
+
+console.log(blackColor.length)
