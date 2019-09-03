6ea503572a8ee9847873edbee403aa3a3553662b
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2019-09-03 13:38:06 -0300

    fixed lenght and width, improved reset btn, added shade btn and party function

--- main.js
+++ main.js
@@ -1,58 +1,52 @@
-let row = 16;
-let j = 0;
-const column = row;
+//Create Grid
 const grid = document.querySelector('.grid');
 
-
-//Create Grid
-
-while (j < row) {
-    for (i = 0; i < column; i++) {
-        const gridElement = document.createElement('div');
-        gridElement.classList.add('grid__element', 'grid__element--hover', 'grid__element--color');
-        grid.setAttribute("style", `grid-template-columns: repeat(${column}, 1fr); grid-template-rows: repeat(${row}, 1fr);`)
-        document.querySelector(".grid").appendChild(gridElement);
+function createDefaultGrid() {
+    let j = 0;
+    let row = 16;
+    const column = row;
+    while (j < row) {
+        for (i = 0; i < column; i++) {
+            const gridElement = document.createElement('div');
+            gridElement.classList.add('grid__element', 'grid__element--hover', 'grid__element--color');
+            grid.setAttribute("style", `grid-template-columns: repeat(${column}, 1fr); grid-template-rows: repeat(${row}, 1fr);`)
+            document.querySelector(".grid").appendChild(gridElement);
+        }
+        j++
     }
-    j++
 }
+createDefaultGrid();
 
 const gridColor = document.querySelector('.grid__element--color');
 const gridElements = document.getElementsByClassName(".grid__element--color");
 
 grid.addEventListener('mouseover', (e) => {
     if (e.target.classList.contains('grid__element')) {
-            e.target.setAttribute('style', 'background-color: black;')
-        }
-    })
+        e.target.setAttribute('style', 'background-color: black;')
+    }
+})
 
 
-// function getRandomColor() {
-
-// }
-
-// function getSelectedColor() {
-
-// }
-
-function resetGrid() {
-
-}
 //Buttons
 
-    // Reset
+// Reset
 
-const reset = document.querySelector(".resetBtn");
-reset.addEventListener('click', () => {
+function resetGrid() {
     while (grid.hasChildNodes()) {
         grid.removeChild(grid.lastChild);
     }
+}
+
+const reset = document.querySelector(".resetBtn");
+reset.addEventListener('click', () => {
     let newGrid = prompt("Enter new value for your grid");
-    if (isNaN(newGrid) || newGrid == ""){
+    if (isNaN(newGrid) || newGrid == "") {
         alert("Invalid value");
         return;
-    } else if( newGrid == null) {
+    } else if (newGrid == null) {
         return;
     } else {
+        resetGrid();
         let j = 0;
         grid.setAttribute('style', `grid-template-rows: repeat(${newGrid}, 1fr); grid-template-columns: repeat(${newGrid}, 1fr);`)
         while (j < newGrid) {
@@ -66,7 +60,7 @@ reset.addEventListener('click', () => {
     }
 })
 
-    //Color button
+//Color buttons
 
 const colorBtn = document.querySelector('.colorBtn')
 colorBtn.addEventListener('change', (e) => {
@@ -74,15 +68,47 @@ colorBtn.addEventListener('change', (e) => {
     grid.addEventListener('mouseover', (e) => {
         if (e.target.classList.contains('grid__element')) {
             e.target.setAttribute('style', `background-color: ${newColor};`)
-            }
-        })
+        }
+    })
 })
 
 const monoBtn = document.querySelector(".monoBtn")
-monoBtn.addEventListener('click', ()=>{
+monoBtn.addEventListener('click', () => {
     grid.addEventListener('mouseover', (e) => {
         if (e.target.classList.contains('grid__element')) {
             e.target.setAttribute('style', `background-color: black;`)
-            }
-        })
+        }
+    })
+})
+
+function getRandomColor() {
+    const hexaCode = "ABCDEF0123456789";
+    let hexaColor = "#";
+    for (i = 0; i < 6; i++) {
+        let randomValue = Math.floor(Math.random() * hexaCode.length)
+        hexaColor += hexaCode.charAt(randomValue);
+    }
+    return hexaColor;
+}
+
+
+const partyBtn = document.querySelector(".partyBtn");
+partyBtn.addEventListener('click', () => {
+    grid.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('grid__element')) {
+            e.target.setAttribute('style', `background-color: ${getRandomColor()}`)
+        }
+    })
+})
+
+const shadeBtn = document.querySelector('.shadeBtn');
+shadeBtn.addEventListener('click', () =>{
+    grid.addEventListener('mouseover', (e) =>{
+        if(e.target.classList.contains("grid__element")){
+            let brightness = 100;
+            e.target.style.filter = `brightness(${brightness - 10}%)`;
+            brightness -= 10;
+            console.log(e.target)
+        }
+    })
 })
